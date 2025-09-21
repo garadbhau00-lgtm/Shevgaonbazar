9024936118bf8d49ade0187c0ffcec04061a576d
Bhau Garad <garadbhau00@gmail.com>
2025-09-21 15:17:46 +0000

    Categories not scrolling horizontal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -171,7 +171,7 @@ export default function Home() {
       <main>
         <div className="sticky top-[96px] z-20 bg-background/95 backdrop-blur-sm p-4 pt-2 pb-2">
             <Tabs value={selectedCategory} onValueChange={setSelectedCategory} className="w-full">
-            <ScrollArea className="w-full whitespace-nowrap" type="scroll" scrollHideDelay={0}>
+            <ScrollArea className="w-full whitespace-nowrap" type="scroll">
               <TabsList className="inline-flex w-max gap-2 bg-transparent p-0">
                 <TabsTrigger value="सर्व" className="h-auto flex flex-col gap-1.5 p-2 text-[11px] rounded-lg">
                   <List className="h-4 w-4" />
