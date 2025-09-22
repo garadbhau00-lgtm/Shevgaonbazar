0b9e37abab8d89cad1ebea7c21597325d2d4251e
Bhau Garad <garadbhau00@gmail.com>
2025-09-22 15:08:39 +0000

    QR show but this is not my provided QR

--- next.config.ts
+++ next.config.ts
@@ -37,7 +37,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'api.qrserver.com',
+        hostname: 'storage.googleapis.com',
         port: '',
         pathname: '/**',
       },
