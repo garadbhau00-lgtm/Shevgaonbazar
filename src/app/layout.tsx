7a842dc9318c2429b7d7c75f83413fcd058eb528
Bhau Garad <garadbhau00@gmail.com>
2025-10-01 06:54:31 +0000

    Still same show

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="mr">
       <head>
+        <title>शेवगाव बाजार</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
